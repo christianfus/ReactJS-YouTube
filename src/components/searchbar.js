3141fb9fc8b5a12fd19749831969c15edef7de82
christianfus <christianfus@gmail.com>
2020-05-03 11:36:00 +0200

    youtube app finished

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 export class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: 'sex' };
+    this.state = { term: '' };
   }
   render() {
     return (
-      <div>
-        <input value={this.state.term} onChange={(event) => this.setState({ term: event.target.value })} />
+      <div className='search-bar'>
+        <input value={this.state.term} onChange={(event) => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange({ term });
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { SearchBar } from './components/searchbar';
 import YTSearch from 'youtube-api-search';
 import { VideoList } from './components/video_list';
 import { VideoDetail } from './components/video_detail';
+import { debounce } from 'lodash';
 
 const YT_API_KEY = 'AIzaSyAlNpkyJSdakYwxCqcYSVNNJfnsSo36g8Q';
 
@@ -11,8 +12,11 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('bitconnect');
+  }
 
-    YTSearch({ key: YT_API_KEY, term: 'bitconnect' }, (videos) => {
+  videoSearch(term) {
+    YTSearch({ key: YT_API_KEY, term }, (videos) => {
       console.log(videos);
 
       this.setState({
@@ -21,12 +25,18 @@ export class App extends Component {
       });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 300);
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} />
+        <SearchBar onSearchTermChange={(term) => videoSearch(term)} />
+        <div className='row'>
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList videos={this.state.videos} onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} />
+        </div>
       </div>
     );
   }
